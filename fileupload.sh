4bdd36aaa626ead33bdee3ec12d7bd8138fa951f
Yinka Bakare <delighting_minds@hotmail.com>
2024-07-20 16:22:33 +0100

    adding fileupload script

---
+++ fileupload.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+FILEPATH=/Users/yinkabakare/downloads
+
+
+read -p " Enter File Name " FILENAME
+
+FULLPATH="$FILEPATH/$FILENAME"
+
+  if [ -f "$FULLPATH" ]; then
+    echo "File provided: $FILENAME"
+  else
+    echo "Error: File does not exist in the downloads folder."
+    exit 1
+fi
+
+
+BUCKET_NAME="fileupload5678483"
+
+
+aws s3 cp "$FULLPATH" "s3://$BUCKET_NAME/$FILENAME"
